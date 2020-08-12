9c25253b4e7e5c0af3ff760d187dddbf4b238b32
Riccardo <riccardofeingold@yahoo.com>
2020-08-12 17:12:35 +0200

    Initial Commit

---
+++ TheButtonSmasher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheButtonSmasher
+//
+//  Created by Riccardo Feingold on 12.08.20.
+//  Copyright © 2020 Riccardo Feingold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
