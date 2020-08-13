80b7e1041ee62b3b2b3515e9cf210428bc8bdaab
Riccardo <riccardofeingold@yahoo.com>
2020-08-13 09:36:37 +0200

    numberOfSmashes stored with User Defaults

--- TheButtonSmasher/Controller/ViewController.swift
+++ TheButtonSmasher/Controller/ViewController.swift
@@ -12,12 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var smashLabel: UILabel!
     
+    //the variable for how many times the button has been pressed
+    var numberOfSmashes = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //retrieve data from .plist; Be sure that you use the same key string when saving and retriving the data otherwise you will get an error
+        numberOfSmashes = UserDefaults.standard.integer(forKey: "smashes")
+
+        smashLabel.text = "Smashes: \(numberOfSmashes)"
     }
     
     @IBAction func buttonSmashed(_ sender: UIButton) {
+        //increase numberOfSmashes by 1
+        numberOfSmashes += 1
+        
+        //Update UITextLabel
+        smashLabel.text = "Smashes: \(numberOfSmashes)"
+        
+        //Save and update the number in the .plist file
+        UserDefaults.standard.set(numberOfSmashes, forKey: "smashes")
     }
 }
 
